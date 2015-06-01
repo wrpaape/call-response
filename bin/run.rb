8c06d90c1e02592eedae7e54713cb223ca8966e1
Justin Herrick <justin@theironyard.com>
2015-06-01 13:30:17 -0500

    Adding bin.

---
+++ bin/run.rb
@@ -0,0 +1,57 @@
+def parse_params(uri_fragments, query_param_string)
+  params = {}
+  params[:resource]  = uri_fragments[3]
+  params[:id]        = uri_fragments[4]
+  params[:action]    = uri_fragments[5]
+  if query_param_string
+    param_pairs = query_param_string.split('&')
+    param_k_v   = param_pairs.map { |param_pair| param_pair.split('=') }
+    param_k_v.each do |k, v|
+      params.store(k.to_sym, v)
+    end
+  end
+  params
+end
+
+def parse(raw_request)
+  pieces = raw_request.split(' ')
+  method = pieces[0]
+  uri    = pieces[1]
+  http_v = pieces[2]
+  route, query_param_string = uri.split('?')
+  uri_fragments = route.split('/')
+  protocol = uri_fragments[0][0..-2]
+  full_url = uri_fragments[2]
+  subdomain, domain_name, tld = full_url.split('.')
+  params = parse_params(uri_fragments, query_param_string)
+  return {
+    method: method,
+    uri: uri,
+    http_version: http_v,
+    protocol: protocol,
+    subdomain: subdomain,
+    domain_name: domain_name,
+    tld: tld,
+    full_url: full_url,
+    params: params
+  }
+end
+
+system('clear')
+loop do
+  print "Supply a valid HTTP Request URL (h for help, q to quit) > "
+  raw_request = gets.chomp
+
+  case raw_request
+  when 'q' then puts "Goodbye!"; exit
+  when 'h'
+    puts "A valid HTTP Request looks like:"
+    puts "\t'GET http://localhost:3000/students HTTP/1.1'"
+    puts "Read more at : http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html"
+  else
+    REQUEST = parse(raw_request)
+    # YOUR CODE GOES BELOW HERE
+    #
+    # YOUR CODE GOES ABOVE HERE  ^
+  end
+end
