6006f0decb8e064f3e12e395a27a056d3821da94
Reid Paape <wrpaape@gmail.com>
2015-06-02 10:28:38 -0500

    progress on nightmare, code prob broken

--- bin/run.rb
+++ bin/run.rb
@@ -2,16 +2,21 @@ require_relative '../db/setup'
 require_relative '../lib/user'
 # Remember to put the requires here for all the classes you write and want to use
 
-def parse_params(uri_fragments, query_param_string)
+def parse_params(method, uri_fragments, query_param_string)
   params = {}
   params[:resource]  = uri_fragments[3]
   params[:id]        = uri_fragments[4]
   params[:action]    = uri_fragments[5]
   if query_param_string
-    param_pairs = query_param_string.split('&')
-    param_k_v   = param_pairs.map { |param_pair| param_pair.split('=') }
-    param_k_v.each do |k, v|
-      params.store(k.to_sym, v)
+    case method
+    when "POST" || "PUTS"
+      param_pairs = query_param_string.split(',')
+      param_k_v   = param_pairs.map { |param_pair| param_pair.split(':') }
+      param_k_v.each { |k, v| params.store(k.to_sym, v) }
+    else
+      param_pairs = query_param_string.split('&')
+      param_k_v   = param_pairs.map { |param_pair| param_pair.split('=') }
+      param_k_v.each { |k, v| params.store(k.to_sym, v.delete('"')) }
     end
   end
   params
@@ -20,14 +25,25 @@ end
 def parse(raw_request)
   pieces = raw_request.split(' ')
   method = pieces[0]
-  uri    = pieces[1]
-  http_v = pieces[2]
-  route, query_param_string = uri.split('?')
+  case method
+  when "POST" || "PUTS"
+    route  = pieces[1]
+    query_param_string = pieces[2].delete("'")
+    http_v = pieces[3]
+  else
+    uri    = pieces[1]
+    http_v = pieces[2]
+    route, query_param_string = uri.split('?')
+  end
+
+
+
+
   uri_fragments = route.split('/')
   protocol = uri_fragments[0][0..-2]
   full_url = uri_fragments[2]
   subdomain, domain_name, tld = full_url.split('.')
-  params = parse_params(uri_fragments, query_param_string)
+  params = parse_params(method, uri_fragments, query_param_string)
   return {
     method: method,
     uri: uri,
